1708591d4244ddc44e0d16de3071ecdf881f3ee1
Bryson Kruk <brysonmk1984@gmail.com>
2023-02-22 10:53:44 -0700

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 mod math;
+use clap::Parser;
 use math::hex_to_rgb::convert_hex_to_rgb;
 use math::rgb_to_hex::convert_rgb_to_hex;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Optional name to operate on
+    name: Option<String>,
+}
+
 fn main() {
+    let cli = Cli::parse();
+
+    // You can check the value provided by positional arguments, or option arguments
+    if let Some(name) = cli.name.as_deref() {
+        println!("Value for name: {name}");
+    }
+
     // HEX TO RGB
     let my_hex = "0A543F";
     let rgb_tuple = convert_hex_to_rgb(my_hex);
@@ -12,4 +27,15 @@ fn main() {
     let my_rgb = "rgb(6, 58, 119)";
     let converted_hex_value = convert_rgb_to_hex(my_rgb);
     println!("RGB TO HEX: {}", converted_hex_value);
+
+    println!("Hello {:?}!", cli.name)
+
+
+
+    let leo = 'leo';
+
+
+
+
+
 }
